452af1052dd3d71a928a5a7004d1380f9cd25ade
Mirjavid <mirjavidaa@code.edu.az>
2021-05-16 17:31:16 +0400

    Clonned task

---
+++ Boilerplate/src/index.js
@@ -0,0 +1,7 @@
+import {contex} from "./layout/app";
+import {resize} from "../node_modules/pyp-09-task/js/main.js";
+import './assets/scss/style.scss';
+
+console.log("hello world");
+console.log(contex);
+console.log(resize(12));
